0e4197c5b50e48828d6f5cf979a97c5e5ebd82da
Lav.lee <leelav.viin@gmail.com>
2026-04-29 14:13:33 +0800

    Change title from 'Enrollment Application' to 'Registration Form'

--- apply.php
+++ apply.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enrollment Application | Pines NHS</title>
+    <title>Registration Form | Pines NHS</title>
     
     <link rel="stylesheet" href="assets/css/style.css?v=<?= time(); ?>">
     <script>
